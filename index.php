04635afe4216ae68342987d7ff8adfac9d61756a
adrianjsds <59375261+adrianjsds@users.noreply.github.com>
2021-09-27 22:55:50 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title> practica</title>
+
+  </head>
+  <link rel = "stylesheet" href="css.css">
+  <link rel = "stylesheet" href="style.css">
+  <body>
+
+    <head>
+  <div id="principal" class="principal" > 
+  <div id="cabecera" class="cabecera" > <span class="icon-home"></span> <div id="buscador" class="buscador" >   
+    <input type="text" class="texto1" id="texto1" placeholder="buscar" onclick="buscar">
+    </div>
+    <div id="itmens" class="itemns" >   
+    
+    
+    </div>
+  </div>
+
+    <li  class="cabecera2">
+<a style="color:#FFF" href="html2/index2.php"><span class="icon-home"></span></a>
+ <a style="color:#FFF" href="html2/index2.php">vender</a>
+  <a style="color:#FFF" href="html2/index2.php">menu2</a>
+</li>
+
+  </head> 
+
+  <section>
+ <div id="recomendados"  class="recomendados">
+  <div id="recomendado1" class="recomendado" > 
+   
+  
+    <img src="imagenes/imagen1.jpg" id="imagen" class="recomendado_2" >
+   
+   </div>
+  
+   <div id="recomendado2" class="recomendado" > 
+     <img src="imagenes/imagen1.jpg" id="imagen" class="recomendado_2" >  
+       
+     </div>
+    <div id="recomendado3" class="recomendado" > 
+    <img src="imagenes/imagen1.jpg" id="imagen" class="recomendado_2" >  
+   </div>
+   </div>
+   </section>
+ 
+  
+
+  <div class="d" id="d"> Redes sociales:
+  <span class=informacion> Información personal:</span>
+<hr>
+   <a href=mailto: "infobelleble@gmail.com">Correo: infobelleble@gmail.com</a>
+    <span class=instagram><a href=" https:www.instagram.com/belleble_/ "> Instagram: belleble_</a></span>
+     <span class=informacion3> Información personal:
+<hr class=linea>
+    </span>
+<span class=informacion2> Estefhany Laurent - Dermocosmeatra - Fisioterapeuta - filantropa - y un coñazo e vergas más         </div> 
+   
+ 
+  </div>
+  </body>
+     <script src=""></script>
+  </html>
